7ef9cd8f314faea2a939c5d54387d4959d4a8485
abdelmajidafaki <157015165+abdelmajidafaki@users.noreply.github.com>
2024-07-17 13:10:04 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,45 @@
-from  flask import Flask,render_template
+from  flask import Flask,render_template,request,flash
+from flask_sqlalchemy import SQLAlchemy
 webapp=Flask(__name__)
-from flask_bootstrap import Bootstrap5
-@webapp.route("/")
-def regesterpage():
+webapp.secret_key = 'test'
+webapp.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/python_project"
+db = SQLAlchemy(webapp)
+class users(db.Model):
+    __tablename__ = 'users'
+    userid = db.Column(db.Integer, primary_key=True)
+    fulname = db.Column(db.String(255), nullable=False)  
+    email = db.Column(db.String(255), nullable=False)
+    Pasword = db.Column(db.String(255), nullable=False)
+@webapp.route("/register", methods=['GET', 'POST'])
+def registerpage():
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        email = request.form['email']
+        password = request.form['password']
+        cpassword=request.form['cpassword']
+        if password==cpassword:
+            new_user = users(fulname=fullname, email=email, Pasword=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Registration successful!", 'success')
+        else:
+            flash("Passwords do not match. Please try again.", 'primary')   
+        return render_template("registerpage.html")
     return render_template("registerpage.html")
+@webapp.route("/login", methods=['GET', 'POST'])
+def loginpage():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        
+        # Query the database to check if the user exists
+        user = users.query.filter_by(email=email, Pasword=password).first()
+        
+        if user:
+            flash("Login successful!", 'success')
+        else:
+            flash("Invalid email or password. Please try again.", 'danger')
+    
+    return render_template("loginpage.html")
 if __name__ == '__main__':
     webapp.run(debug=True)
